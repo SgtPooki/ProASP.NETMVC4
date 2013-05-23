41ae68313265d6b87559930349928d6a9c683ef2
Russell Dempsey <rdempsey@nerdery.com>
2013-05-23 11:09:46 -0500

    finished ninject examples in chapter 6

---
+++ EssentialTools/EssentialTools/Infrastrcture/NinjectDependencyResolver.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Ninject;
+using Ninject.Parameters;
+using Ninject.Syntax;
+using System.Configuration;
+using EssentialTools.Models;
+
+
+namespace EssentialTools.Infrastrcture
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver()
+        {
+            this.kernel = new StandardKernel();
+            this.AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return this.kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return this.kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+            this.kernel.Bind<IValueCalculator>().To<LinqValueCalculator>();
+            this.kernel
+                .Bind<IDiscountHelper>()
+                .To<DefaultDiscountHelper>()
+                .WithConstructorArgument("discount", 50M);
+            this.kernel
+                .Bind<IDiscountHelper>()
+                .To<FlexibleDiscountHelper>()
+                .WhenInjectedInto<LinqValueCalculator>();
+        }
+    }
+}
---
+++ EssentialTools/EssentialTools/Models/Discount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EssentialTools.Models
+{
+    public interface IDiscountHelper
+    {
+        decimal ApplyDiscount(decimal total);
+    }
+
+    public class DefaultDiscountHelper : IDiscountHelper
+    {
+        public decimal DiscountSize;
+
+        public DefaultDiscountHelper(decimal discount)
+        {
+            this.DiscountSize = discount;
+        }
+
+        public decimal ApplyDiscount(decimal total)
+        {
+            return (total - (this.DiscountSize / 100M * total));
+        }
+    }
+}
