cb2f721dff770d221407de88ee47d7eb9d45b6e3
Russell Dempsey <rdempsey@nerdery.com>
2013-05-28 11:53:33 -0500

    finished chapter 7

--- SportsStore/SportsStore.UnitTests/UnitTest1.cs
+++ SportsStore/SportsStore.UnitTests/UnitTest1.cs
@@ -1,14 +1,92 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Controllers;
+using SportsStore.WebUI.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using SportsStore.WebUI.HtmlHelpers;
 
 namespace SportsStore.UnitTests
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        private Mock<IProductsRepository> mockProductRepo = new Mock<IProductsRepository>();
+
+        public UnitTest1()
         {
+            this.mockProductRepo.Setup(mockedObj => mockedObj.Products)
+                .Returns(new Product[] {
+                    new Product {ProductID = 1, Name = "P1"},
+                    new Product {ProductID = 2, Name = "P2"},
+                    new Product {ProductID = 3, Name = "P3"},
+                    new Product {ProductID = 4, Name = "P4"},
+                    new Product {ProductID = 5, Name = "P5"}
+                }.AsQueryable());
         }
+
+        [TestMethod]
+        public void canPaginate()
+        {
+            //arrange
+            ProductController controller = new ProductController(this.mockProductRepo.Object);
+            controller.productsPerPage = 3;
+
+            //act
+            ProductsListViewModel result = (ProductsListViewModel)controller.List(2).Model;
+
+            //assert
+            Product[] prodArray = result.Products.ToArray();
+            Assert.AreEqual(prodArray.Length, 2);
+            Assert.AreEqual(prodArray[0].Name, "P4");
+            Assert.AreEqual(prodArray[1].Name, "P5");
+        }
+
+        [TestMethod]
+        public void canGeneratePageLinks()
+        {
+            //arrange
+            HtmlHelper myHelper = null;
+
+            PagingInfo pagingInfo = new PagingInfo
+            {
+                CurrentPage = 2,
+                TotalItems = 28,
+                ItemsPerPage = 10
+            };
+
+            Func<int, string> pageUrlDelegate = (i => "Page" + i);
+
+            //act
+            MvcHtmlString result = myHelper.PageLinks(pagingInfo, pageUrlDelegate);
+
+            //assert
+            Assert.AreEqual(result.ToString(), @"<a href=""Page1"">1</a>"
+                                             + @"<a class=""selected"" href=""Page2"">2</a>"
+                                             + @"<a href=""Page3"">3</a>");
+        }
+
+        [TestMethod]
+        public void canSendPageViewModel()
+        {
+            //Arrange            
+            ProductController controller = new ProductController(this.mockProductRepo.Object);
+            controller.productsPerPage = 3;
+
+            //act
+            ProductsListViewModel result = (ProductsListViewModel)controller.List(2).Model;
+
+            //assert
+            PagingInfo pageInfo = result.PagingInfo;
+            Assert.AreEqual(pageInfo.CurrentPage, 2);
+            Assert.AreEqual(pageInfo.ItemsPerPage, 3);
+            Assert.AreEqual(pageInfo.TotalItems, 5);
+            Assert.AreEqual(pageInfo.TotalPages, 2);
+        }
+
     }
 }
--- SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
@@ -5,21 +5,36 @@ using System.Web;
 using System.Web.Mvc;
 using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Models;
 
 namespace SportsStore.WebUI.Controllers
 {
     public class ProductController : Controller
     {
         private IProductsRepository repository;
+        public int productsPerPage = 4;
 
         public ProductController(IProductsRepository productRepo)
         {
             this.repository = productRepo;
         }
 
-        public ViewResult List()
+        public ViewResult List(int page = 1)
         {
-            return View(this.repository.Products);
+            ProductsListViewModel model = new ProductsListViewModel
+            {
+                Products = this.repository.Products
+                    .OrderBy(p => p.ProductID)
+                    .Skip((page - 1) * this.productsPerPage)
+                    .Take(this.productsPerPage),
+                PagingInfo = new PagingInfo {
+                    CurrentPage = page,
+                    ItemsPerPage = this.productsPerPage,
+                    TotalItems = this.repository.Products.Count()
+                }
+            };
+
+            return View(model);
         }
 
     }
